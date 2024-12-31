c9e8a230e8735773df23e9450509954a29aad46a
Nitikajsp <nitika@jspinfotech.com>
2024-12-31 11:39:25 +0530

    Route setting

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,10 +21,22 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        // echo '<pre>';
+        // print_r($user->role_id);
+        // echo '</pre>';
+        // dd($user);
+        if ($user->role_id == 1) {
+            return view('admin.dashboard');
+        } elseif ($user->role_id == 2) {
+            return view('employee.dashboard');
+        }
+
+        return view('dashboard');
     }
 
-    
+
 }
