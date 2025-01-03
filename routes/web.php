d2858606f3ef9dd20a8ef7fdaf07d0c6a09816ea
Nitikajsp <nitika@jspinfotech.com>
2025-01-03 17:03:40 +0530

    punch time issue fixed

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\LeaveController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('dashboard');
-// });
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
@@ -27,7 +27,8 @@ Route::get('/', function () {
     return view('auth.login');
 });
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+    Route::get('/employee/dashboard', [HomeController::class, 'index'])->name('employee.dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -55,6 +56,9 @@ Route::middleware('auth')->group(function () {
     Route::get('my-leave/{id}/edit', [LeaveController::class, 'edit'])->name('my-leave.edit');
     Route::post('my-leave/{id}/update', [LeaveController::class, 'update'])->name('my-leave.update');
     Route::delete('attendancemy-leave/{id}', [LeaveController::class, 'destroy'])->name('my-leave.destroy');
+
+    Route::post('/save-punch-data', [AttendanceController::class, 'savePunchData'])->name('savePunchData');
+
 
 });
 
